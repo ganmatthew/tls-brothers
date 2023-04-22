d2c595a5ab99fd647b35b76948918ad31fd0d9af
Matthew Gan <24535419+ganmatthew@users.noreply.github.com>
2023-04-22 13:15:55 +0800

    Communities carousel layout optimizations

--- php/components/communities.php
+++ php/components/communities.php
@@ -1,18 +1,17 @@
 <div class="comm_carousel comm_pady">
-    <div class="container comm_card">
-        <div class="card_left col-sm-12 col-lg-6 d-flex flex-column">
-            <img width="400px" src="assets/1911.png">
+    <div class="container comm_card d-flex flex-wrap justify-content-center align-items-center">
+        <!--Image-->
+        <div class="card_left col-sm-12 col-lg-6 d-flex flex-column align-items-center">
+            <img src="assets/1911.png">
         </div>
+        <!--Info-->
         <div class="card_right col-sm-12 col-lg-6 d-flex flex-column">
-            <!--Title-->
-            <h3>1911 Brothers' Community</h3>
-            <!--Location, number of brothers-->
+            <h3 id="comm-name">1911 Brothers' Community</h3>
             <div class="card_info">
-                <p id="location"><strong>Location: </strong>De La Salle University Manila, Philippines (4F St. La Salle Hall)</p>
-                <p id="num-brothers"><strong>Number of Brothers assigned: </strong>8</p>
+                <p id="comm-location"><strong>Location: </strong>De La Salle University Manila, Philippines (4F St. La Salle Hall)</p>
+                <p id="comm-num-brothers"><strong>Number of Brothers assigned: </strong>8</p>
             </div>
-            <!--Description-->
-            <p id="card-desc">
+            <p id="comm-desc">
                 The Brothers assigned to this community serve in De La Salle University - Manila, De La Salle - College of St. Benilde, De La Salle Santiago Zobel, St Jaime Hilario School - De La Salle Bataan, and De La Salle Araneta University. The community' name came from the foundation year of the first La Salle school in the Philippines. This is also the country's first community of De La Salle Brothers.
             </p>
         </div>
