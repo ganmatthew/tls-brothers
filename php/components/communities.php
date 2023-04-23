bf8580a10f08f7c6c817c01c45e504c6468d0abe
Matthew Gan <24535419+ganmatthew@users.noreply.github.com>
2023-04-24 03:16:15 +0800

    Partial implementation of carousel for three communities

--- php/components/communities.php
+++ php/components/communities.php
@@ -1,20 +1,81 @@
 <div class="comm_carousel comm_pady">
-    <div class="container comm_card d-flex flex-wrap justify-content-center align-items-center">
-        <!--Image-->
-        <div class="card_left col-sm-12 col-lg-6 d-flex flex-column align-items-center">
-            <img src="assets/1911.png">
-        </div>
-        <!--Info-->
-        <div class="card_right col-sm-12 col-lg-6 d-flex flex-column">
-            <h3 id="comm-name">1911 Brothers' Community</h3>
-            <div class="card_info">
-                <p id="comm-location"><strong>Location: </strong>De La Salle University Manila, Philippines (4F St. La Salle Hall)</p>
-                <p id="comm-num-brothers"><strong>Number of Brothers assigned: </strong>8</p>
+    <div id="comm-carousel" class="carousel slide" data-bs-wrap="true">
+        <!--
+        <ol class="carousel-indicators">
+            <li data-target="#comm-carousel" data-slide-to="0" class="active"></li>
+            <li data-target="#comm-carousel" data-slide-to="1"></li>
+        </ol>
+        -->
+        <div class="carousel inner">
+            <!--1: 1911-->
+            <div class="carousel-item active">
+                <div class="comm_card d-flex flex-wrap justify-content-center align-items-center">
+                    <!--Image-->
+                    <div class="card_left col-sm-12 col-lg-6 d-flex flex-column align-items-center">
+                        <img src="assets/1911.png">
+                    </div>
+                    <!--Info-->
+                    <div class="card_right col-sm-12 col-lg-6 d-flex flex-column">
+                        <h3 id="comm-name">1911 Brothers' Community</h3>
+                        <div class="card_info">
+                            <p id="comm-location"><strong>Location: </strong>De La Salle University - Manila, Manila, Philippines</p>
+                            <p id="comm-num-brothers"><strong>Number of Brothers assigned: </strong>8</p>
+                        </div>
+                        <p id="comm-desc">
+                            The Brothers assigned to this community serve in De La Salle University - Manila, De La Salle - College of St. Benilde, De La Salle Santiago Zobel, St Jaime Hilario School - De La Salle Bataan, and De La Salle Araneta University. The community' name came from the foundation year of the first La Salle school in the Philippines. This is also the country's first community of De La Salle Brothers.
+                        </p>
+                    </div>
+                </div>
             </div>
-            <p id="comm-desc">
-                The Brothers assigned to this community serve in De La Salle University - Manila, De La Salle - College of St. Benilde, De La Salle Santiago Zobel, St Jaime Hilario School - De La Salle Bataan, and De La Salle Araneta University. The community' name came from the foundation year of the first La Salle school in the Philippines. This is also the country's first community of De La Salle Brothers.
-            </p>
+            <!--2: St Benilde-->
+            <div class="carousel-item">
+                <div class="comm_card d-flex flex-wrap justify-content-center align-items-center">
+                    <!--Image-->
+                    <div class="card_left col-sm-12 col-lg-6 d-flex flex-column align-items-center">
+                        <img src="">
+                    </div>
+                    <!--Info-->
+                    <div class="card_right col-sm-12 col-lg-6 d-flex flex-column">
+                        <h3 id="comm-name">St. Benilde Brothers' Community</h3>
+                        <div class="card_info">
+                            <p id="comm-location"><strong>Location: </strong>La Salle Green Hills, Mandaluyong, Philippines</p>
+                            <p id="comm-num-brothers"><strong>Number of Brothers assigned: </strong>8</p>
+                        </div>
+                        <p id="comm-desc">
+                            The Brothers assigned to this community serve in La Salle Green Hills, De La Salle - College of St Benilde, and La Salle College Antipolo.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <!--3: St Jaime Hilario-->
+            <div class="carousel-item">
+                <div class="comm_card d-flex flex-wrap justify-content-center align-items-center">
+                    <!--Image-->
+                    <div class="card_left col-sm-12 col-lg-6 d-flex flex-column align-items-center">
+                        <img src="">
+                    </div>
+                    <!--Info-->
+                    <div class="card_right col-sm-12 col-lg-6 d-flex flex-column">
+                        <h3 id="comm-name">St. Jaime Hilario Brothers' Community</h3>
+                        <div class="card_info">
+                            <p id="comm-location"><strong>Location: </strong>St. Jaime Hilario School - De La Salle Bataan, Bagac, Bataan, Philippines</p>
+                            <p id="comm-num-brothers"><strong>Number of Brothers assigned: </strong>3</p>
+                        </div>
+                        <p id="comm-desc">
+                            The Brothers assigned to this community serve in St Jaime Hilario School - De La Salle Bataan.
+                        </p>
+                    </div>
+                </div>
+            </div>
         </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#comm-carousel" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#comm-carousel" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
     </div>
 </div>
 
