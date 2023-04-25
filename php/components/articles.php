835d7290fb413180bc95f86950fcb717909a9a5a
Matthew Gan <24535419+ganmatthew@users.noreply.github.com>
2023-04-25 23:53:11 +0800

    Made article cards clickable + added shadows with hover transition

--- php/components/articles.php
+++ php/components/articles.php
@@ -7,28 +7,31 @@
     <!-- Article Cards -->
     <div class="article row px-auto mx-auto">
         <div class="card col-sm">
-            <img class="card-img-top ac-visual" src="assets/Lasallian_Brothers_Art.png" alt="Lasallian Brothers Art">
-            <div class="card-body">
-                <h5 class="card-title justify-content-center align-items-center text-center"><strong>Best of both worlds: Into the De La Salle Brothers' artistic hobbies</strong></h5>
-                <p class="card-text justify-content-center align-items-center text-center">For Br. Dodo, Br. Mario, and Br. Manny, art allows them to channel inner peace and their passion for positive social change.</p>
-                <a href="https://thelasallian.com/2022/05/20/best-of-both-worlds-into-the-de-la-salle-brothers-artistic-hobbies/" class="card-link">Read More ></a>
-            </div>
+            <a class="card-link" href="https://thelasallian.com/2022/05/20/best-of-both-worlds-into-the-de-la-salle-brothers-artistic-hobbies/">
+                <img class="card-img-top ac-visual" src="assets/Lasallian_Brothers_Art.png" alt="Lasallian Brothers Art">
+                <div class="card-body">
+                    <h5 class="card-title justify-content-center align-items-center text-center"><strong>Best of both worlds: Into the De La Salle Brothers' artistic hobbies</strong></h5>
+                    <p class="card-text justify-content-center align-items-center text-center">For Br. Dodo, Br. Mario, and Br. Manny, art allows them to channel inner peace and their passion for positive social change.</p>
+                </div>
+            </a>
         </div>
         <div class="card col-sm">
-            <img class="card-img-top ac-visual" src="assets/DeLaSalleBrothers.jpg" alt="Card image cap">
-            <div class="card-body">
-                <h5 class="card-title justify-content-center align-items-center text-center"><strong>The De La Salle Brothers of the Philippines</strong></h5>
-                <p class="card-text justify-content-center align-items-center text-center"></p>
-                <a href="https://thelasallian.com/2013/08/07/the-de-la-salle-brothers-of-the-philippines/" class="card-link">Read More ></a>
-            </div>
+            <a class="card-link" href="https://thelasallian.com/2013/08/07/the-de-la-salle-brothers-of-the-philippines/">
+                <img class="card-img-top ac-visual" src="assets/DeLaSalleBrothers.jpg" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title justify-content-center align-items-center text-center"><strong>The De La Salle Brothers of the Philippines</strong></h5>
+                    <p class="card-text justify-content-center align-items-center text-center">Learn more about the De La Salle Brothers in the Philippines.</p>
+                </div>
+            </a>
         </div>
         <div class="card col-sm">
-            <img class="card-img-top ac-visual" src="assets/Archiving-the-lasallian-mission.png" alt="Archiving the Lasallian Mission">
-            <div class="card-body">
-                <h5 class="card-title justify-content-center align-items-center text-center"><strong>Archiving the Lasallian mission in the Philippines</strong></h5>
-                <p class="card-text justify-content-center align-items-center text-center"></p>
-                <a href="https://thelasallian.com/2022/10/26/archiving-the-lasallian-mission-in-the-philippines/" class="card-link">Read More ></a>
-            </div>
+            <a class="card-link" href="https://thelasallian.com/2022/10/26/archiving-the-lasallian-mission-in-the-philippines/">
+                <img class="card-img-top ac-visual" src="assets/Archiving-the-lasallian-mission.png" alt="Archiving the Lasallian Mission">
+                <div class="card-body">
+                    <h5 class="card-title justify-content-center align-items-center text-center"><strong>Archiving the Lasallian mission in the Philippines</strong></h5>
+                    <p class="card-text justify-content-center align-items-center text-center">Since their arrival on Philippine soil in 1911, the De La Salle Brothers have made their presence felt for over a century.</p>
+                </div>
+            </a>
         </div>
     </div>
 </div>
