0c812dd8b22cbe3fcd67d81f228d24ba1aa329f9
Matthew Gan <24535419+ganmatthew@users.noreply.github.com>
2023-04-28 02:08:46 +0800

    Added all timeline entries

--- php/components/timeline.php
+++ php/components/timeline.php
@@ -19,31 +19,136 @@
    <!--Timeline proper-->
    <div class="card-container">
       <div class="timeline-card">
-         <img src="assets/timeline/dls_Canon_Hotelbg-2.jpg"></img>
+         <img src="assets/timeline/timeline1.jpg"></img>
          <div class="text-container">
             <h2>1651</h2>
             <p>A man named John Baptist de La Salle was born on April 30, 1651 in Reims, Champagne, France. Being born into a noble family, he was able to attain higher education that was difficult to acquire at the time due to high poverty rates. De La Salle dreamed of and eventually became a priest.</p>
          </div>
       </div>
       <div class="timeline-card">
-         <img src="assets/timeline/dls_Canon_Hotelbg-2.jpg"></img>
+         <img src="assets/timeline/timeline2.jpg"></img>
          <div class="text-container">
             <h2>1679</h2>
             <p>John Baptist de La Salle encountered Adrian Nyel, a layman who aims to provide schooling for the poor, while working with the Sisters of the Child Jesus. After hearing Nyel’s intentions and his request for help, De La Salle pursued to be involved in the education for the poor which ended up become his life's work.</p>
          </div>
       </div>
       <div class="timeline-card">
-         <img src="assets/timeline/dls_Canon_Hotelbg-2.jpg"></img>
+         <img src="assets/timeline/timeline3.jpg"></img>
          <div class="text-container">
             <h2>1680-1682</h2>
             <p>De La Salle took steps in order to train a small group of teachers that lacked purpose and leadership. He invited them to eat at his home and eventually brought them to live with him. Due to the disturbed feelings of his family, he and his fellow teachers moved to a poor part of the city.</p>
          </div>
       </div>
       <div class="timeline-card">
-         <img src="assets/timeline/dls_Canon_Hotelbg-2.jpg"></img>
+         <img src="assets/timeline/timeline4.jpg"></img>
          <div class="text-container">
             <h2>1683</h2>
             <p>The teaching community under De La Salle’s guidance started to become more formalized. Around this time, De La Salle started to call this community “Brothers.” To be fully in one with his Brothers, he distributed all his wealth to the poor during a winter famine.</p>
+         </div>
+      </div>
+      <div class="timeline-card">
+         <img src="assets/timeline/timeline5.jpg"></img>
+         <div class="text-container">
+            <h2>1686</h2>
+            <p>The institute's name "Brothers of the Christian Schools" was officially adopted, along the wearing of the distinctive Brothers' habit as well as the vow of obedience.</p>
+         </div>
+      </div>
+      <div class="timeline-card">
+         <img src="assets/timeline/timeline6.jpeg" style="max-height: max-content;"></img>
+         <div class="text-container">
+            <h2>1691</h2>
+            <p>Amidst the difficulties the Brothers and De La Salle himself are facing, the Heroic Vow was made between De La Salle and his two most trusted Brothers, Nicholas Vuyart and Gabriel Drolin, to "remain together in society even if they would have to beg and live on bread alone."</p>
+         </div>
+      </div>
+      <div class="timeline-card">
+         <img src="assets/timeline/timeline7.jpg"></img>
+         <div class="text-container">
+            <h2>1694</h2>
+            <p>Perpetual vows of obedience, stability, and association for the educational service of the poor were taken by De La Salle and the Brothers.</p>
+         </div>
+      </div>
+      <div class="timeline-card">
+         <img src="assets/timeline/timeline8.jpg"></img>
+         <div class="text-container">
+            <h2>1719</h2>
+            <p>From age, sickness, and tireless labors, John Baptist de La Salle died at the age of 67 on a Good Friday on April 7, 1719. The work he left behind continued through the Brothers of the Christian Schools establishing La Salle schools worldwide.</p>
+         </div>
+      </div>
+      <div class="timeline-card">
+         <img src="assets/timeline/timeline9.jpg"></img>
+         <div class="text-container">
+            <h2>1900</h2>
+            <p>On May 24, 1900, John Baptist de La Salle was canonized as a saint by Pope Leo XIII.</p>
+         </div>
+      </div>
+      <div class="timeline-card">
+         <img src="assets/timeline/timeline10.jpg"></img>
+         <div class="text-container">
+            <h2>1911</h2>
+            <p>After the liberation of the Philippines from Spain, Archbishop of Manila Jeremiah Harty turned to the Brothers of the Christian Schools in order to introduce English-based Catholic education in the Philippines. With this—on June 19, 1911—9 Christian Brothers from Europe and the United States opened the first La Salle school in the Philippines at Nozaleda Street, Paco, Manila.</p>
+         </div>
+      </div>
+      <div class="timeline-card">
+         <img src="assets/timeline/timeline11.jpg"></img>
+         <div class="text-container">
+            <h2>1912</h2>
+            <p>On February 12, 1912, the first La Salle school in the Philippines was established as De La Salle College (DLSC), and was authorized to grant high school diplomas.</p>
+         </div>
+      </div>
+      <div class="timeline-card">
+         <img src="assets/timeline/timeline12.jpg"></img>
+         <div class="text-container">
+            <h2>1921</h2>
+            <p>De La Salle College transferred from Paco to its current location in Taft Avenue, Malate due to the rising student population. Throughout the construction of the new building, classes were being conducted in it already. Besides primary and secondary levels, tertiary education was in the works through a two-year Commerce program. </p>
+         </div>
+      </div>
+      <div class="timeline-card">
+         <img src="assets/timeline/timeline13.jpg"></img>
+         <div class="text-container">
+            <h2>1941-1943</h2>
+            <p>DLSC and the Christian Brothers were allowed to continue teaching in the midst of World War II. They opened to teach students from other schools that closed down, even while Japanese forces occupied a part of the campus as their South Manila defense quarters. The college discontinued classes in 1943 and transferred students to St. Scholastica’s College.</p>
+         </div>
+      </div>
+      <div class="timeline-card">
+         <img src="assets/timeline/timeline14.jpg"></img>
+         <div class="text-container">
+            <h2>1945</h2>
+            <p>Japanese troops attacked DLSC and massacred 41 individuals seeking refuge in the campus in February 1945. Out of the 17 resident Christian Brothers, only one of them survived, forcing the closure of De La Salle College. Primary and secondary levels resumed classes in July of the same year despite the lack of manpower and facilities as a result of the war.</p>
+         </div>
+      </div>
+      <div class="timeline-card">
+         <img src="assets/timeline/timeline15.jpg"></img>
+         <div class="text-container">
+            <h2>1950</h2>
+            <p>In 1950, Pope Pius XII proclaimed St. John Baptist de La Salle as Patron Saint of Teachers from inspirational writings on his life.</p>
+         </div>
+      </div>
+      <div class="timeline-card">
+         <img src="assets/timeline/timeline16.jpg" style="max-height: max-content;"></img>
+         <div class="text-container">
+            <h2>1969</h2>
+            <p>The feast day of St. La Salle was moved to April 7, the day of his death or his 'birth into heaven'.</p>
+         </div>
+      </div>
+      <div class="timeline-card">
+         <img src="assets/timeline/timeline17.jpg"></img>
+         <div class="text-container">
+            <h2>1975</h2>
+            <p>On February 19, 1975, De La Salle College was granted university status and was renamed to De La Salle University (DLSU).</p>
+         </div>
+      </div>
+      <div class="timeline-card">
+         <img src="assets/timeline/timeline18.jpg"></img>
+         <div class="text-container">
+            <h2>1978</h2>
+            <p>To once again deal with the rising student population, the Christian Brothers opened De La Salle Santiago Zobel (DLS-Z) in Alabang, Muntinlupa. With the transferring of primary and secondary education to DLS-Z, DLSU became a sole higher-education institution.</p>
+         </div>
+      </div>
+      <div class="timeline-card">
+         <img src="assets/timeline/timeline19.jpg"></img>
+         <div class="text-container">
+            <h2>2011</h2>
+            <p>The Brothers of the Christian Schools celebrate 100 years of Lasallian presence in the Philippines.</p>
          </div>
       </div>
    </div>
@@ -54,7 +159,6 @@
    let starLogo = document.getElementById('nav-logo');
    
    let logoType = 0;
-
    function toggleLogo() {
       if (logoType == 0) {
          logoType++;
