2c34a6160ac18758b531dc175a571de2170caa90
Matthew Gan <24535419+ganmatthew@users.noreply.github.com>
2023-04-29 00:57:23 +0800

    Set Br. Bernie as active + added tooltip text on vox

--- php/components/vox.php
+++ php/components/vox.php
@@ -8,110 +8,110 @@
     
     <div class="vox-icon px-auto mx-auto">
         <div class="vox-img" style="--i:1;" data-id="content1">
-            <img src="assets/vox-populi/brother1.jpg" alt="Brother #">
+            <img src="assets/vox-populi/brother1.jpg" title="Br. Dennis Magbanua, FSC">
         </div>
         <div class="vox-img" style="--i:2;" data-id="content2">
-            <img src="assets/vox-populi/brother2.jpg" alt="Brother #">
+            <img src="assets/vox-populi/brother2.jpg" title="Br. Jose Mari Jimenez, FSC">
         </div>
-        <div class="vox-img" style="--i:3;" data-id="content3">
-            <img src="assets/vox-populi/brother3.jpg" alt="Brother #">
+        <div class="vox-img vox-active" style="--i:3;" data-id="content3">
+            <img src="assets/vox-populi/brother3.jpg" title="Br. Bernard Oca, FSC">
         </div>
         <div class="vox-img" style="--i:4;" data-id="content4">
-            <img src="assets/vox-populi/brother4.jpg" alt="Brother #">
+            <img src="assets/vox-populi/brother4.jpg" title="Br. Martin Sellner, FSC">
         </div>
         <div class="vox-img" style="--i:5;" data-id="content5">
-            <img src="assets/vox-populi/brother5.jpg" alt="Brother #">
+            <img src="assets/vox-populi/brother5.jpg" title="Br. Raymundo Suplido, FSC">
         </div>
         <div class="vox-img" style="--i:6;" data-id="content6">
-            <img src="assets/vox-populi/brother6.jpg" alt="Brother #">
+            <img src="assets/vox-populi/brother6.jpg" title="Br. Antonio Servando, FSC">
         </div>
         <div class="vox-img" style="--i:7;" data-id="content7">
-            <img src="assets/vox-populi/brother7.jpg" alt="Brother #">
+            <img src="assets/vox-populi/brother7.jpg" title="Br. Emmanuel Sia, FSC">
         </div>
         <div class="vox-img" style="--i:8;" data-id="content8">
-            <img src="assets/vox-populi/brother8.jpg" alt="Brother #">
+            <img src="assets/vox-populi/brother8.jpg" title="Br. Kino Escolano, FSC">
         </div>
         <div class="vox-img" style="--i:9;" data-id="content9">
-            <img src="assets/vox-populi/brother9.jpg" alt="Brother #">
+            <img src="assets/vox-populi/brother9.jpg" title="Br. Weegee Enriquez, FSC">
         </div>
         <div class="vox-img" style="--i:10;" data-id="content10">
-            <img src="assets/vox-populi/brother10.jpg" alt="Brother #">
+            <img src="assets/vox-populi/brother10.jpg" title="Br. Richie Yap, FSC">
         </div>
         <div class="vox-img" style="--i:11;" data-id="content11">
-            <img src="assets/vox-populi/brother11.jpg" alt="Brother #">
+            <img src="assets/vox-populi/brother11.jpg" title="Br. Sockie de la Rosa, FSC">
         </div>
         <div class="vox-img" style="--i:12;" data-id="content12">
-            <img src="assets/vox-populi/brother12.jpg" alt="Brother #">
+            <img src="assets/vox-populi/brother12.jpg" title="Br. Arian Lopez, FSC">
         </div>
         <div class="vox-img" style="--i:13;" data-id="content13">
-            <img src="assets/vox-populi/brother13.jpg" alt="Brother #">
+            <img src="assets/vox-populi/brother13.jpg" title="Br. Mikey Cua, FSC">
         </div>
     </div>
 
     <div class="vox-content px-auto mx-auto">
         <div class="vox-contentBx" id="content1">
             <div class="vox-img">
-                <img src="assets/vox-populi/brother1.jpg" alt="Brother #">
+                <img src="assets/vox-populi/brother1.jpg" title="Br. Dennis Magbanua, FSC">
             </div>
         </div>
         <div class="vox-contentBx" id="content2">
             <div class="vox-img">
-                <img src="assets/vox-populi/brother2.jpg" alt="Brother #">
+                <img src="assets/vox-populi/brother2.jpg" title="Br. Jose Mari Jimenez, FSC">
             </div>
         </div>
-        <div class="vox-contentBx" id="content3">
+        <div class="vox-contentBx vox-active" id="content3">
             <div class="vox-img">
-                <img src="assets/vox-populi/brother3.jpg" alt="Brother #">
+                <img src="assets/vox-populi/brother3.jpg" title="Br. Bernard Oca, FSC">
             </div>
         </div>
         <div class="vox-contentBx" id="content4">
             <div class="vox-img">
-                <img src="assets/vox-populi/brother4.jpg" alt="Brother #">
+                <img src="assets/vox-populi/brother4.jpg" title="Br. Martin Sellner, FSC">
             </div>
         </div>
         <div class="vox-contentBx" id="content5">
             <div class="vox-img">
-                <img src="assets/vox-populi/brother5.jpg" alt="Brother #">
+                <img src="assets/vox-populi/brother5.jpg" title="Br. Raymundo Suplido, FSC">
             </div>
         </div>
         <div class="vox-contentBx" id="content6">
             <div class="vox-img">
-                <img src="assets/vox-populi/brother6.jpg" alt="Brother #">
+                <img src="assets/vox-populi/brother6.jpg" title="Br. Antonio Servando, FSC">
             </div>
         </div>
         <div class="vox-contentBx" id="content7">
             <div class="vox-img">
-                <img src="assets/vox-populi/brother7.jpg" alt="Brother #">
+                <img src="assets/vox-populi/brother7.jpg" title="Br. Emmanuel Sia, FSC">
             </div>
         </div>
         <div class="vox-contentBx" id="content8">
             <div class="vox-img">
-                <img src="assets/vox-populi/brother8.jpg" alt="Brother #">
+                <img src="assets/vox-populi/brother8.jpg" title="Br. Kino Escolano, FSC">
             </div>
         </div>
         <div class="vox-contentBx" id="content9">
             <div class="vox-img">
-                <img src="assets/vox-populi/brother9.jpg" alt="Brother #">
+                <img src="assets/vox-populi/brother9.jpg" title="Br. Weegee Enriquez, FSC">
             </div>
         </div>
         <div class="vox-contentBx" id="content10">
             <div class="vox-img">
-                <img src="assets/vox-populi/brother10.jpg" alt="Brother #">
+                <img src="assets/vox-populi/brother10.jpg" title="Br. Richie Yap, FSC">
             </div>
         </div>
         <div class="vox-contentBx" id="content11">
             <div class="vox-img">
-                <img src="assets/vox-populi/brother11.jpg" alt="Brother #">
+                <img src="assets/vox-populi/brother11.jpg" title="Br. Sockie de la Rosa, FSC">
             </div>
         </div>
         <div class="vox-contentBx" id="content12">
             <div class="vox-img">
-                <img src="assets/vox-populi/brother12.jpg" alt="Brother #">
+                <img src="assets/vox-populi/brother12.jpg" title="Br. Arian Lopez, FSC">
             </div>
         </div>
         <div class="vox-contentBx" id="content13">
             <div class="vox-img">
-                <img src="assets/vox-populi/brother13.jpg" alt="Brother #">
+                <img src="assets/vox-populi/brother13.jpg" title="Br. Mikey Cua, FSC">
             </div>
         </div>
     </div>
@@ -121,43 +121,43 @@
 <!-- quote cards here -->
 <div class="popup-image-container">
     <div class="popup-image">
-        <img src="assets/brocards/1.png" alt="card #">
+        <img src="assets/brocards/1.png" alt="">
     </div>
     <div class="popup-image">
-        <img src="assets/brocards/2.png" alt="card #">
+        <img src="assets/brocards/2.png" alt="">
     </div>
     <div class="popup-image">
-        <img src="assets/brocards/3.png" alt="card #">
+        <img src="assets/brocards/3.png" alt="">
     </div>
     <div class="popup-image">
-        <img src="assets/brocards/4.png" alt="card #">
+        <img src="assets/brocards/4.png" alt="">
     </div>
     <div class="popup-image">
-        <img src="assets/brocards/5.png" alt="card #">
+        <img src="assets/brocards/5.png" alt="">
     </div>
     <div class="popup-image">
-        <img src="assets/brocards/6.png" alt="card #">
+        <img src="assets/brocards/6.png" alt="">
     </div>
     <div class="popup-image">
-        <img src="assets/brocards/7.png" alt="card #">
+        <img src="assets/brocards/7.png" alt="">
     </div>
     <div class="popup-image">
-        <img src="assets/brocards/8.png" alt="card #">
+        <img src="assets/brocards/8.png" alt="">
     </div>
     <div class="popup-image">
-        <img src="assets/brocards/9.png" alt="card #">
+        <img src="assets/brocards/9.png" alt="">
     </div>
     <div class="popup-image">
-        <img src="assets/brocards/10.png" alt="card #">
+        <img src="assets/brocards/10.png" alt="">
     </div>
     <div class="popup-image">
-        <img src="assets/brocards/11.png" alt="card #">
+        <img src="assets/brocards/11.png" alt="">
     </div>
     <div class="popup-image">
-        <img src="assets/brocards/12.png" alt="card #">
+        <img src="assets/brocards/12.png" alt="">
     </div>
     <div class="popup-image">
-        <img src="assets/brocards/13.png" alt="card #">
+        <img src="assets/brocards/13.png" alt="">
     </div>
     <div class="popup-close">
         <span>&times;</span>
