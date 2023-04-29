5ad3ecb9e255bdb3cdefb0a97e047d1621beb3c6
Matthew Gan <24535419+ganmatthew@users.noreply.github.com>
2023-04-30 00:59:40 +0800

    Tweak timeline title + address further grammatical errors

--- php/components/timeline.php
+++ php/components/timeline.php
@@ -1,7 +1,7 @@
 <div class="timeline-section">
    <div class="timeline-background d-flex flex-column align-items-center justify-content-center">
       <h1>371 years in the making</h1>
-      <p>Learn about the origins of the Lasallian mission from where it all started, and how it made its way to the Philippines.</p>
+      <p>Learn about the origins of the Lasallian mission from where it all started and how it made its way to the Philippines.</p>
       <button type="button" class="open-button" onclick="toggleTimeline()">View</button>
    </div>
 </div>
@@ -14,8 +14,8 @@
    <!--Title-->
    <div class="timeline-title">
       <h1>
-         <span class="title-top">HISTORY OF</span><br>
-         <span class="title-bottom">THE DE LA SALLE BROTHERS</span>
+         <span class="title-top">HISTORY OF THE</span><br>
+         <span class="title-bottom">DE LA SALLE BROTHERS</span>
       </h1>
    </div>
    <!--Timeline proper-->
@@ -24,28 +24,28 @@
          <img src="assets/timeline/timeline1.jpg"></img>
          <div class="text-container">
             <h2>1651</h2>
-            <p>A man named John Baptist de La Salle was born on April 30, 1651 in Reims, Champagne, France. Being born into a noble family, he was able to attain higher education that was difficult to acquire at the time due to high poverty rates. De La Salle dreamed of and eventually became a priest.</p>
+            <p>John Baptist de La Salle was born on April 30, 1651 in Reims, Champagne, France. Being born into a noble family, he was able to attain higher education, which was a great privilege at the time due to prevalent poverty. De La Salle dreamed of and eventually became a priest.</p>
          </div>
       </div>
       <div class="timeline-card">
          <img src="assets/timeline/timeline2.jpg"></img>
          <div class="text-container">
             <h2>1679</h2>
-            <p>John Baptist de La Salle encountered Adrian Nyel, a layman who aims to provide schooling for the poor, while working with the Sisters of the Child Jesus. After hearing Nyel’s intentions and his request for help, De La Salle pursued to be involved in the education for the poor which ended up becoming his life's work.</p>
+            <p>John Baptist de La Salle encountered Adrian Nyel, a layman who aimed to provide schooling for the poor, while working with the Sisters of the Child Jesus. After hearing Nyel’s intentions and his request for help, De La Salle pursued to be involved in the education for the poor which eventually became his life's work.</p>
          </div>
       </div>
       <div class="timeline-card">
          <img src="assets/timeline/timeline3.jpg"></img>
          <div class="text-container">
             <h2>1680-1682</h2>
-            <p>De La Salle took steps in order to train a small group of teachers that lacked purpose and leadership. He invited them to eat at his home and eventually brought them to live with him. Due to the disturbed feelings of his family, he and his fellow teachers moved to a poor part of the city.</p>
+            <p>De La Salle took steps in order to train a group of teachers who lacked purpose and leadership. He invited them to eat at his home and eventually brought them to live with him. Due to the disturbed feelings of his family, he and his fellow teachers moved to a poor part of the city.</p>
          </div>
       </div>
       <div class="timeline-card">
          <img src="assets/timeline/timeline4.jpg"></img>
          <div class="text-container">
             <h2>1683</h2>
-            <p>The teaching community under De La Salle’s guidance started to become more formalized. Around this time, De La Salle started to call this community “Brothers”. To be fully in one with his Brothers, he distributed all his wealth to the poor during a winter famine.</p>
+            <p>The teaching community under De La Salle's guidance started to become more formalized. Around this time, De La Salle started to call this community “Brothers”. To be fully one with his Brothers, he distributed all his wealth to the poor during a winter famine.</p>
          </div>
       </div>
       <div class="timeline-card">
@@ -59,14 +59,14 @@
          <img src="assets/timeline/timeline6.jpeg" style="max-height: max-content;"></img>
          <div class="text-container">
             <h2>1691</h2>
-            <p>Amid the difficulties the Brothers and De La Salle himself were facing, the Heroic Vow was made between De La Salle and his two most trusted Brothers, Nicholas Vuyart and Gabriel Drolin, to "remain together in society even if they would have to beg and live on bread alone."</p>
+            <p>As De La Salle and the Brothers faced financial difficulties, the Heroic Vow was made between De La Salle and his two most trusted Brothers, Nicholas Vuyart and Gabriel Drolin, to "remain together in society even if they would have to beg and live on bread alone."</p>
          </div>
       </div>
       <div class="timeline-card">
          <img src="assets/timeline/timeline7.jpg"></img>
          <div class="text-container">
             <h2>1694</h2>
-            <p>Perpetual vows of obedience, stability, and association for the educational service of the poor were taken by De La Salle and the Brothers.</p>
+            <p>De La Salle and the Brothers made a perpetual vow of obedience, stability, and association for the educational service of the poor.</p>
          </div>
       </div>
       <div class="timeline-card">
