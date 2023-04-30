55b94a8f26c0ba1ccf69e714302ad8a4c3bfe76c
Matthew Gan <24535419+ganmatthew@users.noreply.github.com>
2023-04-30 16:26:46 +0800

    Fix scaling of timeline section

--- php/components/timeline.php
+++ php/components/timeline.php
@@ -1,5 +1,5 @@
 <div class="timeline-section">
-   <div class="timeline-background d-flex flex-column align-items-center justify-content-center">
+   <div class="timeline-background">
       <h1>371 years in the making</h1>
       <p>Learn about the origins of the Lasallian mission from where it all started and how it made its way to the Philippines.</p>
       <button type="button" class="open-button" onclick="toggleTimeline()">View</button>
